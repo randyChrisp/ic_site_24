755c708543188efc95171ff7cd673f1bd7ee5e93
randyChrisp <79rchrisp@gmail.com>
2024-03-31 21:26:12 -0400

    Added about us (mobile)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>&#169; 2024 INNOVATIVE CONNEXIONS LLC</footer>;
+  return (
+    <footer>
+      <p>&#169; 2024 INNOVATIVE CONNEXIONS LLC</p>
+      <ul className="footer-icons">
+        <li>
+          <a href="https://www.facebook.com/webwondersinc" target="_blank">
+            <i className="fab fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/Web_Wonders_Inc" target="_blank">
+            <i className="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/web_wonders_inc/" target="_blank">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </li>
+        <li>
+          <a href="www.linkedin.com/in/randy-chrisp" target="_blank">
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
